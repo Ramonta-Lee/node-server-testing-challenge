3a9b85479a1a7eaf374c82bb1b0f5c332dacae70
unknown <viola4lfe@gmail.com>
2020-02-27 17:58:31 -0500

    able to create a resource and remove a resource from the database

--- user/user-model.js
+++ user/user-model.js
@@ -1,5 +1,30 @@
-
+const db = require("../database/dbConfig.js");
 
 module.exports = {
-addUser
+  addUser,
+  find,
+  findById,
+  remove
+};
+
+async function addUser(user) {
+  const [id] = await db("users").insert(user, "id");
+
+  return findById(id);
+}
+
+function find() {
+  return db("users");
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+function remove(id) {
+  return db("users")
+    .where(id, "id")
+    .del();
 }
--- user/user-router.js
+++ user/user-router.js
@@ -1,15 +1,42 @@
+const router = require("express").Router();
 const Users = require("./user-model.js");
 
-const router = require("express").Router();
-
-router.post("/", (req, res) => {
-  Users.all()
+router.get("/", (req, res) => {
+  Users.find()
     .then(user => {
       res.status(200).json(user);
     })
-    .catch(({ name, message, stack, code }) => {
-      console.log(name, message, stack, code);
-      res.status(500).json({ name, message, stack, code });
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "Unable to get list of users" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const name = req.body;
+  // console.log(req.body);
+  if (!name) {
+    res.status(401).json({ error: "please provide a name" });
+  } else {
+    Users.addUser(name)
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(({ name, message, stack, code }) => {
+        console.log(name, message, stack, code);
+        res.status(500).json({ name, message, stack, code });
+      });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params;
+  Users.remove(id)
+    .then(user => {
+      res.status(200).json({ message: "user has been removed" });
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Unable to remove the user" });
     });
 });
 
